389968ee252fd64e07dc4fa98b37e36b01d074f2
Timothy <madindoustephen8@gmail.com>
2025-11-03 14:13:49 +0300

    postgresql and redis

--- properties/models.py
+++ properties/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class Property(models.Model):
-    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)  # Title of the property
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=255)  # Location of the property
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.title} - {self.location}"
